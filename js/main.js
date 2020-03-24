4afad1260de8e4c55a753d2d4da28f24f4d6e072
bhavayAnand9 <bhavayanandcse@gmail.com>
2020-03-25 01:37:43 +0530

    optimization

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-$('document').ready(function () {
-  let url = "https://play.google.com/store/apps/collection/topselling_free?clp=ChcKFQoPdG9wc2VsbGluZ19mcmVlEAcYAw%3D%3D:S:ANO1ljLwMrI&gsr=ChkKFwoVCg90b3BzZWxsaW5nX2ZyZWUQBxgD:S:ANO1ljIxP20";
-  $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', function(data){
-    let dataset = [];
+
+//to deal with cors
+const topAppsurl = "https://play.google.com/store/apps/collection/topselling_free?clp=ChcKFQoPdG9wc2VsbGluZ19mcmVlEAcYAw%3D%3D:S:ANO1ljLwMrI&gsr=ChkKFwoVCg90b3BzZWxsaW5nX2ZyZWUQBxgD:S:ANO1ljIxP20";
+
+//set because all elements will always be unique
+let dataset = new Set();
+
+//to keep track of new items
+let titleDataSet = new Set();
+
+//runs when document is ready and loaded into DOM
+function ready() {
+  $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(topAppsurl) + '&callback=?', function(data){
     let t = $(data.contents).find('div.Vpfmgd');
      for(let i=0; i<t.length; i++){
        let item = {
@@ -9,34 +18,79 @@ $('document').ready(function () {
          img: $(t[i]).find('img.T75of').attr('data-src'),
          title: $(t[i]).find('div.WsMG1c').text(),
        };
-       dataset.push(item);
+       titleDataSet.add(item.title);
+       dataset.add(item);
      }
+     buildUpWebPage(dataset);
+  });
+}
 
-     for(let i=0; i<dataset.length; i++){
-      //  let card = `
-      //   <div class="card" style="width: 18rem;">
-      //     <img src="${dataset[i].img}" class="card-img-top" alt="...">
-      //     <div class="card-body">
-      //       <p class="card-text">${dataset[i].title}</p>
-      //     </div>
-      //   </div>
-      // `;
-       let card = `
+//a one timer function to load all children into DOM
+function buildUpWebPage(){
+  let datasetArray = Array.from(dataset);
+  for(let i=0; i<datasetArray.length; i++){
+    let card = `
        <div class="col-auto mb-3">
             <div class="card" style="width: 15rem;">
                 <div class="card-body">
-                    <img src="${dataset[i].img}" class="card-img-top" alt="...">
-                    <h5 class="card-title">${dataset[i].title}</h5>
-                    <a href=${dataset[i].link} class="card-link">More Details</a>
+                    <img src="${datasetArray[i].img}" class="card-img-top" alt="...">
+                    <h5 class="card-title">${datasetArray[i].title}</h5>
+                    <a href=${datasetArray[i].link} class="card-link">More Details</a>
                 </div>
             </div>
         </div>
       `;
-       $("div.prnt").append(card);
-     }
+    $("div.prnt").append(card);
+  }
+}
 
+/*
+  runs only when you have few new apps to append instead of looping through all data and appending it to DOM
+  complexity depends number of new items in trending page
+*/
+function appendNewParticularChildrens(newItems) {
+  let newItemsArray = Array.from(newItems);
+  for(let i=0; i<newItemsArray.length; i++){
+    let card = `
+       <div class="col-auto mb-3">
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <img src="${newItemsArray[i].img}" class="card-img-top" alt="...">
+                    <h5 class="card-title">${newItemsArray[i].title}</h5>
+                    <a href=${newItemsArray[i].link} class="card-link">More Details</a>
+                </div>
+            </div>
+        </div>
+      `;
+    $("div.prnt").append(card);
+  }
+}
+
+/*
+  when data reset => web page scraped => checks if titleDataSet not contains this record => add new item to onlyNewItems set
+  => call appendNewParticularChildrens so only new items will be appended
+*/
+function resetData(){
+  $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(topAppsurl) + '&callback=?', function(data){
+    let onlyNewItems = new Set();
+    let t = $(data.contents).find('div.Vpfmgd');
+    for(let i=0; i<t.length; i++){
+      let item = {
+        link: $(t[i]).find('a.poRVub').attr('href'),
+        img: $(t[i]).find('img.T75of').attr('data-src'),
+        title: $(t[i]).find('div.WsMG1c').text(),
+      };
+      if(!titleDataSet.has(item.title)){
+        onlyNewItems.add(item);
+        titleDataSet.add(item.title);
+        dataset.add(item);
+      }
+    }
+    appendNewParticularChildrens(onlyNewItems);
+
+    //clear onlyNewItems because items are no longer new to us, they have been added to DOM
+    onlyNewItems.clear();
   });
+}
 
-
-
-});
+$('document').ready(ready);
